017b0b5e44a26d907e8021768c369606ef5951b9
Darío Kondratiuk <dariokondratiuk@gmail.com>
2019-04-29 08:51:34 -0300

    Dollar Example

--- news/routes/dolar.js
+++ news/routes/dolar.js
@@ -1,11 +1,18 @@
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
-var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const puppeteer = require('puppeteer');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.json({ dolar: 43.2 });
+router.get('/', async function(req, res, next) {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto('https://www.ambito.com/contenidos/dolar-oficial.html');
+    const cotizacion = await page.evaluate(() => document.querySelector(".data-venta").innerText);
+    await browser.close();
+
+    res.json({ dolar: cotizacion });
 });
 
 module.exports = router;
